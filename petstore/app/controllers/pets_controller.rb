6bdbe7562030eab51fb8fecb47a5661ef4b82ec1
Brandon <kounse@gmail.com>
2023-04-17 19:30:01 -0500

    Added new and create methods as well as pet_params in pet controller. Adjusted index view for pet to change buttons to links

--- petstore/app/controllers/pets_controller.rb
+++ petstore/app/controllers/pets_controller.rb
@@ -1,4 +1,23 @@
 class PetsController < ApplicationController
   def index
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet.params)
+    if @pet.save
+      redirect_to @pet
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :type, :age)
+  end
 end
