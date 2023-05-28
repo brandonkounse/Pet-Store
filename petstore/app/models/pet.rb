b35bc7f7ed62e0e71fdb8109361f639bad132077
Brandon Kounse <kounse@gmail.com>
2023-05-28 08:07:34 -0500

    Added logic to clear the cache key if the model was updated.

--- petstore/app/models/pet.rb
+++ petstore/app/models/pet.rb
@@ -1,3 +1,11 @@
 class Pet < ApplicationRecord
   validates :age, numericality: { only_integer: true }
+
+  after_update_commit :clear_cache
+
+  private
+
+  def clear_cache
+    Rails.cache.delete("pet#{id}")
+  end
 end
