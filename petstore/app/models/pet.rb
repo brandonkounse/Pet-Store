b4cc0db74b33fac06abfacf69f53ceaf317324b4
Brandon Kounse <kounse@gmail.com>
2023-05-29 09:43:03 -0500

    Added more validation logic to catch improper saves from being sent to the database

--- petstore/app/models/pet.rb
+++ petstore/app/models/pet.rb
@@ -1,7 +1,10 @@
 class Pet < ApplicationRecord
-  validates :age, numericality: { only_integer: true }
+  validates :age, numericality: { only_integer: true }, presence: true
+  validates :species, presence: true
+  validates :name, presence: true
+  validates :price, numericality: { less_than_or_equal_to: 999.99, greater_than: 0 }, presence: true
 
-  after_commit :clear_cache, :on => [:create, :update, :destroy]
+  after_commit :clear_cache, on: [:create, :update, :destroy]
 
   private
 
