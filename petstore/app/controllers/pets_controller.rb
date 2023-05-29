53a31d8b05763b1749db7465589fe0072b88bda1
Brandon Kounse <kounse@gmail.com>
2023-05-28 20:15:07 -0500

    added price to params that are permitted

--- petstore/app/controllers/pets_controller.rb
+++ petstore/app/controllers/pets_controller.rb
@@ -61,6 +61,6 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-    params.require(:pet).permit(:name, :species, :age)
+    params.require(:pet).permit(:name, :species, :age, :price)
   end
 end
