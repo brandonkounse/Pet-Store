b591987f59b06c61e2bb41e63c7c55cb8e9f757a
Brandon Kounse <kounse@gmail.com>
2023-05-31 18:40:24 -0500

    Added custom configuration for order routing

--- petstore/config/routes.rb
+++ petstore/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'search', to: 'pets#search'
 
   resources :pets
-  resources :stores, path: 'store'
+  resources :stores, path: 'store' do
+    get 'order', on: :member
+  end
 
   root 'stores#index'
 end
