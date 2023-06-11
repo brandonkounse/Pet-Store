50e26990da569ed09448e7585166208a2e21a2e3
Brandon Kounse <kounse@gmail.com>
2023-06-11 18:41:01 -0500

    Refactoring and rebuilding how urls work

--- petstore/app/controllers/stores_controller.rb
+++ petstore/app/controllers/stores_controller.rb
@@ -2,12 +2,4 @@ class StoresController < ApplicationController
   def index
     @pet = Pet.all
   end
-
-  def order
-    @pet = Rails.cache.fetch("pet#{params[:id]}", expires_in: 30.minutes) do
-      Pet.find(params[:id])
-    end
-
-    render :order
-  end
 end
