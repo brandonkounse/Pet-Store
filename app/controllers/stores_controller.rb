fbbe8bf1fc5e1b480a5b86e88c4e730333f203e4
Brandon Kounse <kounse@gmail.com>
2023-06-14 18:06:28 -0500

    Added methods for routing

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,4 +2,21 @@ class StoresController < ApplicationController
   def index
     @pet = Pet.all
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+  end
 end
