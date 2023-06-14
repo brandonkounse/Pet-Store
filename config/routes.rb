748d3d48003510020106620c454da092849c7453
Brandon Kounse <kounse@gmail.com>
2023-06-14 18:07:57 -0500

    Added route to change store/new to store/order

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'stores#index'
 
   get 'search', to: 'pets#search'
+  get '/store/order', to: 'stores#new'
 
   resources :pets
   resources :stores, path: 'store'
-
 end
