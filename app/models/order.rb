39592bd64290d74aa5e16da2185c832d4606b149
Brandon Kounse <kounse@gmail.com>
2023-06-16 16:29:22 -0500

    Created logic for generating order numbers as well as the order date and removing pet from store on successful order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,22 @@
 class Order < ApplicationRecord
+  before_create :generate_order_number, :set_order_date
+  after_commit :delete_pet
+
+  private
+
+  def generate_order_number
+    loop do
+      self.order_number = SecureRandom.alphanumeric(10).upcase
+      break unless Order.exists?(order_number: order_number)
+    end
+  end
+
+  def set_order_date
+    self.order_date = Time.zone.now
+  end
+
+  def delete_pet
+    pet_id = JSON.parse(order_details)["id"]
+    Pet.find_by(id: pet_id)&.destroy
+  end
 end
