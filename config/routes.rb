93a2c72bed2b6a2204068b432bdc8c45486275da
Brandon Kounse <kounse@gmail.com>
2023-06-16 16:30:54 -0500

    Added specified routes under resources for stores

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'stores#index'
 
   get 'search', to: 'pets#search'
-  get '/store/order', to: 'stores#new'
+  # get '/store/order', to: 'stores#new'
 
   resources :pets
-  resources :stores, path: 'store'
+  resources :stores, path: 'store' do
+    get 'order', to: 'stores#new', on: :collection
+    post 'order', to: 'stores#create', on: :collection
+  end
 end
