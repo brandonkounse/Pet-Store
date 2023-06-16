b7d70deb0bc20b1fbcbbfc20e93c2af0d6d4c5ca
Brandon Kounse <kounse@gmail.com>
2023-06-16 16:28:35 -0500

    Added logic for Pet on new orders. Created redirect for successful create. Added order_params private method

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,11 +4,17 @@ class StoresController < ApplicationController
   end
 
   def new
+    @pet = Pet.find(params[:id])
     @order = Order.new
   end
 
   def create
-    @order = Order.new
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to stores_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -18,5 +24,11 @@ class StoresController < ApplicationController
   def destroy
     @order = Order.find(params[:id])
     @order.destroy
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_email, :total_cost, :order_details)
   end
 end
