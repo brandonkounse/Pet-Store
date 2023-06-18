40dffbbccfb8e899be6a7f850460aec28e5a6d75
Brandon Kounse <kounse@gmail.com>
2023-06-18 13:32:30 -0500

    created order controller and moved most of the order logic over to this controller from store

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+  def new
+    @pet = Pet.find(params[:id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to @order
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_email, :total_cost, :order_details)
+  end
+end
