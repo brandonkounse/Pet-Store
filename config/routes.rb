ce7327454376da613a5b090d7776a74db958d351
Brandon Kounse <kounse@gmail.com>
2023-06-17 21:06:55 -0500

    removed commented out line

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'stores#index'
 
   get 'search', to: 'pets#search'
-  # get '/store/order', to: 'stores#new'
 
   resources :pets
   resources :stores, path: 'store' do
