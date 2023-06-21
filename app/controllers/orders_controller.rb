1d1d0a9ff82528316d476eb8368637b3ddd4940f
Brandon Kounse <kounse@gmail.com>
2023-06-21 11:04:54 -0500

    Added pet_sold? method for a redirect so the same pet couldn't be ordered more than once. Used session object to persist pet data between http requests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,18 @@
 class OrdersController < ApplicationController
   def new
-    @pet = Pet.find(params[:id])
+    session[:pet] = params[:id]
+    @pet = Pet.find(session[:pet])
     @order = Order.new
+    pet_sold?
   end
 
   def create
+    @pet = Pet.find(session[:pet])
     @order = Order.new(order_params)
     if @order.save
       redirect_to @order
     else
-      render :new, status: :unprocessable_entity
+      render :new, params: [session[:pet]], status: :unprocessable_entity
     end
   end
 
@@ -22,9 +25,21 @@ class OrdersController < ApplicationController
     @order.destroy
   end
 
+  def sold
+    @pet = Pet.find(session[:pet])
+  end
+
   private
 
   def order_params
     params.require(:order).permit(:user_email, :total_cost, :order_details)
+  end
+
+  def pet_sold?
+    if @pet.sold == true
+      redirect_to :sold
+    else
+      render :new
+    end
   end
 end
