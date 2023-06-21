b6ffc5cc00788e81894d5cd8235bc12c5f8943e0
Brandon Kounse <kounse@gmail.com>
2023-06-21 16:54:28 -0500

    created an inventory path for store, as well as view and controller action

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,4 +2,8 @@ class StoresController < ApplicationController
   def index
     @pet = Pet.all
   end
+
+  def inventory
+    @pet = Pet.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'stores#index'
 
   get 'search', to: 'pets#search'
+  get 'store/inventory', to: 'stores#inventory'
   get 'sold', to: 'orders#sold'
 
   resources :pets
   resources :stores, path: 'store', only: [:index]
-  resources :orders, path: 'store/order', only: [:index, :new, :create, :show, :destroy]
+  resources :orders, path: 'store/order', only: [:new, :create, :show, :destroy]
 end
