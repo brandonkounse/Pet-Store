9c6e2d60954c7c4cdde8f09ec00045d1227cd272
Brandon Kounse <kounse@gmail.com>
2023-07-19 15:28:24 -0500

    added second test and renamed it blocks

--- spec/controllers/store_spec.rb
+++ spec/controllers/store_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.describe StoresController do
   describe 'limiter' do
-    it 'blocks > 5 requests per second' do
+    before(:each) do
+      sleep(1)
+    end
+
+    it 'allows get requests <= 4 per second' do
+      4.times { get :index }
+      expect(response).to have_http_status(200)
+    end
+
+    it 'blocks get requests > 5 per second' do
       expect { 6.times { get :index } }.to output('Limit reached!').to_stdout
     end
   end
