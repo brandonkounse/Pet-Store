63836852ae654622c9ca240b0303277e027a2b82
Brandon Kounse <kounse@gmail.com>
2023-08-10 23:05:56 -0500

    more endpoint tests

--- spec/controllers/pet_spec.rb
+++ spec/controllers/pet_spec.rb
@@ -17,9 +17,18 @@ RSpec.describe PetsController do
     end
 
     context 'when sending post request to :create' do
-      it 'it successfully redirects with code 302' do
-        post :create, params: { pet: { name: 'test', species: 'tester', age: 1, price: 4.99 } }
-        expect(response).to have_http_status(302)
+      context 'when submitting valid data' do
+        it 'successfully redirects with code 302' do
+          post :create, params: { pet: { name: 'test', species: 'tester', age: 1, price: 4.99 } }
+          expect(response).to have_http_status(302)
+        end
+      end
+
+      context 'when submitting invalid data' do
+        it 'returns status code 422' do
+           post :create, params: { pet: { name: 'test' } }
+           expect(response).to have_http_status(422)
+        end
       end
     end
 
@@ -27,11 +36,34 @@ RSpec.describe PetsController do
       let(:fake_pet) { instance_double('fake_pet', id: 17) }
 
       it 'returns status code 200' do
-        allow(Pet).to receive(:find).and_return(fake_pet.id)
+        allow(Rails.cache).to receive(:fetch).with("pet#{fake_pet.id}", expires_in: 30.minutes).and_return(fake_pet)
         get :show, params: { id: fake_pet.id }
         expect(response).to have_http_status(200)
       end
     end
+
+    context 'when sending get request to :edit' do
+      let(:fake_pet) { instance_double('fake_pet', id: 19) }
+
+      it 'returns status code 200' do
+        allow(Pet).to receive(:find).with(fake_pet.id.to_s).and_return(fake_pet)
+        get :edit, params: { id: fake_pet.id }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when sending put request to :show' do # calling the #update method
+      let(:fake_pet) { Pet.create(id: 1000, name: 'bugs', species: 'bugsby', age: 9, price: 11.99) }
+
+      context 'when submitting valid data' do
+        it 'successfully redirects with code 302' do
+          put :update, params: { id: fake_pet.id, pet: { name: 'test', species: 'tester', age: 1, price: 4.99 } }
+          expect(response).to have_http_status(302)
+        end
+      end
+    end
+  # search
+  # destroy
   end
 
   context 'Rate Limiting' do
@@ -39,7 +71,7 @@ RSpec.describe PetsController do
       sleep(1)
     end
 
-    context 'when sending get requests to :index' do
+    context 'when sending get request to :index' do
       it 'allows requests <= 4 per second' do
         4.times { get :index }
         expect(response).to have_http_status(200)
@@ -50,7 +82,7 @@ RSpec.describe PetsController do
       end
     end
 
-    context 'when sending get requests to :new' do
+    context 'when sending get request to :new' do
       it 'allows requests <= 4 per second' do
         4.times { get :new }
         expect(response).to have_http_status(200)
@@ -61,7 +93,7 @@ RSpec.describe PetsController do
       end
     end
 
-    context 'when sending get requests to :show' do
+    context 'when sending get request to :show' do
       let(:fake_pet) { double('fake_pet', id: 1) }
 
       it 'allows requests <= 4 per second' do
@@ -76,7 +108,7 @@ RSpec.describe PetsController do
       end
     end
 
-    context 'when sending get requests to :edit' do
+    context 'when sending get request to :edit' do
       let(:fake_pet) { double('fake_pet', id: 1) }
 
       it 'allows requests <= 4 per second' do
@@ -91,7 +123,7 @@ RSpec.describe PetsController do
       end
     end
 
-    context 'when sending get requests to :search' do
+    context 'when sending get request to :search' do
       let(:fake_search) { 'sa' }
 
       it 'allows requests <= 4 per second' do
