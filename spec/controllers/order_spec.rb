b2cd329b8cc698c33677a7593bc500ffa76a3d4e
Brandon Kounse <kounse@gmail.com>
2023-08-15 21:27:12 -0500

    added logic for limiter #sold

--- spec/controllers/order_spec.rb
+++ spec/controllers/order_spec.rb
@@ -43,5 +43,23 @@ RSpec.describe OrdersController do
         expect { 6.times { get :show, params: { id: fake_order.id } } }.to output('Limit reached!').to_stdout
       end
     end
+
+    context ':sold' do
+      let(:fake_pet) { double('fake_pet', id: 1) }
+
+      before do
+        allow(Pet).to receive(:find).and_return(fake_pet)
+        allow(fake_pet).to receive(:sold).and_return true
+      end
+
+      it 'allows GET requests <= 4 per second' do
+        4.times { get :sold }
+        expect(response).to have_http_status(200)
+      end
+
+      it 'blocks GET requests > 5 per second' do
+        expect { 6.times { get :sold } }.to output('Limit reached!').to_stdout
+      end
+    end
   end
 end
