cc7229333aae1c213e2f53c60038a3ec418a321c
Brandon Kounse <kounse@gmail.com>
2023-09-10 18:31:40 -0500

    added json response to #search

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -45,25 +45,35 @@ class PetsController < ApplicationController
 
   def update
     @pet = Pet.find(params[:id])
-    if @pet.update(pet_params)
-      redirect_to @pet
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @pet.update(pet_params)
+        format.html { redirect_to @pet }
+        format.json { render json: { name: @pet.name, species: @pet.species, age: @pet.age, price: @pet.price } }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { errors: @pet.errors.full_messages } }
+      end
     end
   end
 
   def search
     @search_query = params[:search]
-    begin
-      @pet = Pet.where('id = ?', Integer(@search_query))
-    rescue ArgumentError
-      @pet = Pet.where('name ILIKE ?', "%#{@search_query}%")
-    end
+    respond_to do |format|
+      begin
+        @pet = Pet.where('id = ?', Integer(@search_query))
+      rescue ArgumentError
+        @pet = Pet.where('name ILIKE ?', "%#{@search_query}%")
+      end
 
-    if @pet.empty?
-      flash.now[:notice] = 'No pets found'
-    elsif @pet.count == 1
-      redirect_to pet_path(@pet.first)
+      if @pet.empty?
+        format.html { flash.now[:notice] = 'No pets found' }
+        format.json { render json: { errors: 'No pets found' } }
+      elsif @pet.count == 1
+        format.html { redirect_to pet_path(@pet.first) }
+        format.json { render json: @pet.first }
+      end
+      format.html
+      format.json { render json: @pet }
     end
   end
 
