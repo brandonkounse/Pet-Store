b1e80ab232a0b90051aef053d13ad77235894841
Brandon Kounse <kounse@gmail.com>
2023-09-18 21:28:50 -0500

    added first round of refactored tests

--- spec/controllers/order_spec.rb
+++ spec/controllers/order_spec.rb
@@ -4,99 +4,40 @@ require 'rails_helper'
 
 RSpec.describe OrdersController do
   context 'Controller Actions' do
-    subject(:fake_order) { double('fake_order') }
-    let(:fake_pet) { double('fake_pet', id: 1, sold: false) }
+    subject(:fake_order) { Order.create(user_email: 'test@email.com', pet_id: 1) }
+    let(:fake_pet) { Pet.create(name: 'test', species: 'test', age: 11, price: 1.99) }
 
-    context 'when sending GET request to #new' do
+    context 'when sending GET request to :new' do
       it 'returns status code 200' do
         allow(Pet).to receive(:find).and_return(fake_pet)
-        allow(fake_pet).to receive(:sold).and_return false
         get :new
         expect(response).to have_http_status(200)
       end
     end
 
-    # create
-    context 'when sending POST request to #create' do
-      context 'when submitting valid data' do
+    context 'when sending POST request to :create' do
+      context 'when valid data is submitted' do
         it 'redirects with status code 302' do
           allow(Pet).to receive(:find).and_return(fake_pet)
-          allow(fake_pet).to receive(:sold).and_return false
-          allow(JSON).to receive(:parse).and_return(fake_pet.id.to_s)
-          post :create, params: {
-            order: {
-              user_email: 'test@email.com'
-              # total_cost: '12.99',
-              # order_details: fake_pet.to_json
-            }
-          }
+          post :create, params: { order: { user_email: 'test@email.com', pet_id: 1 } }
           expect(response).to have_http_status(302)
         end
       end
-    end
-    # show
-    # destroy
-    # sold
-  end
 
-  context 'Rate Limiting' do
-    before(:each) do
-      sleep(1)
-    end
-
-    context ':new' do
-      let(:fake_pet) { double('fake_pet', id: 1) }
-
-      before do
-        allow(Pet).to receive(:find).and_return(fake_pet)
-        allow(fake_pet).to receive(:sold).and_return false
+      context 'when invalid data is submitted' do
+        it 'returns status unprocessable entity' do
+          allow(Pet).to receive(:find).and_return(fake_pet)
+          post :create, params: { order: { user_email: 'test.com', pet_id: 1 } }
+          expect(response).to have_http_status(422)
+        end
       end
+    end
 
-      it 'allows GET requests <= 4 per second' do
-        4.times { get :new, params: { id: fake_pet.id } }
+    context 'when sending GET request to :show' do
+      it 'returns status code 200' do
+        allow(Order).to receive(:find).and_return(fake_order)
+        get :show, params: { id: 1 }
         expect(response).to have_http_status(200)
-      end
-
-      it 'blocks GET requests > 5 per second' do
-        expect { 6.times { get :new, params: { id: fake_pet.id } } }.to output('Limit reached!').to_stdout
-      end
-    end
-
-    context ':show' do
-      let(:fake_pet) { double('fake_pet', id: 1) }
-      let(:fake_order) { double('fake_order', id: 1) }
-
-      before do
-        allow(Pet).to receive(:find).and_return(fake_pet)
-        allow(Rails.cache).to receive(:fetch).with("order#{fake_order.id}",
-                                                   expires_in: 30.minutes).and_return(fake_order)
-      end
-
-      it 'allows GET requests <= 4 per second' do
-        4.times { get :show, params: { id: fake_order.id } }
-        expect(response).to have_http_status(200)
-      end
-
-      it 'blocks GET requests > 5 per second' do
-        expect { 6.times { get :show, params: { id: fake_order.id } } }.to output('Limit reached!').to_stdout
-      end
-    end
-
-    context ':sold' do
-      let(:fake_pet) { double('fake_pet', id: 1) }
-
-      before do
-        allow(Pet).to receive(:find).and_return(fake_pet)
-        allow(fake_pet).to receive(:sold).and_return true
-      end
-
-      it 'allows GET requests <= 4 per second' do
-        4.times { get :sold }
-        expect(response).to have_http_status(200)
-      end
-
-      it 'blocks GET requests > 5 per second' do
-        expect { 6.times { get :sold } }.to output('Limit reached!').to_stdout
       end
     end
   end
