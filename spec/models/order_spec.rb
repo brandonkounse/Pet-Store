47f039f46c777d471b553313ed4f2894e006f018
Brandon Kounse <kounse@gmail.com>
2023-09-21 18:41:28 -0500

    re-wrote tests for refactored model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,43 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :order do
+  let(:pet) { Pet.create(age: 1, name: 'test', species: 'tester', price: 100, id: 1) }
+
+  before do
+    allow(Pet).to receive(:find).and_return(pet)
+  end
+
   context :email do
     it 'validates the presence of user email' do
       order = Order.new
-      expect(order.valid?).to be_falsey
+      expect(order).to_not be_valid
       expect(order.errors[:user_email]).to include("can't be blank")
     end
 
-    it 'validates correct email address syntax' do
-      order = Order.new(user_email: 'test@email.com')
-      expect(order.valid?).to be_truthy
+    it 'invalidates incorrect email address syntax' do
+      order = Order.create(user_email: 'test123', pet_id: 1)
+      expect(order).to_not be_valid
+      expect(order.errors[:user_email]).to include('is invalid')
     end
 
-    it 'invalidates incorrect email address syntax' do
-      order = Order.create(user_email: 'test123')
-      expect(order.valid?).to be_falsey
-      expect(order.errors[:user_email]).to include('is invalid')
+    it 'validates correct email address syntax' do
+      order = Order.new(user_email: 'test@email.com', pet_id: 1)
+      expect(order).to be_valid
     end
   end
 
   context :callbacks do
     it 'generates order number before_create' do
-      pet = Pet.create(age: 1, name: 'test', species: 'tester', price: 100)
-      order = Order.new(user_email: 'test@email.com', order_details: "{\"id\": #{pet.id}}")
+      order = Order.new(user_email: 'test@email.com', pet_id: 1)
       expect(order).to receive(:generate_order_number)
       order.save
     end
 
     it 'generates set order date before_create' do
-      pet = Pet.create(age: 1, name: 'test', species: 'tester', price: 100)
-      order = Order.new(user_email: 'test@email.com', order_details: "{\"id\": #{pet.id}}")
+      order = Order.new(user_email: 'test@email.com', pet_id: 1)
       expect(order).to receive(:set_order_date)
       order.save
     end
 
     it 'generates toggle sold after_commit' do
-      pet = Pet.create(age: 1, name: 'test', species: 'tester', price: 100)
-      order = Order.new(user_email: 'test@email.com', order_details: "{\"id\": #{pet.id}}")
+      order = Order.new(user_email: 'test@email.com', pet_id: 1)
       expect(order).to receive(:toggle_sold)
       order.save
     end
