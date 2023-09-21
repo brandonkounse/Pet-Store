fd42cad28edf8db19d36e056d7974fea3804c394
Brandon Kounse <kounse@gmail.com>
2023-09-21 18:13:08 -0500

    changed test desc in rate limit to <=5 per second

--- spec/controllers/pet_spec.rb
+++ spec/controllers/pet_spec.rb
@@ -124,8 +124,8 @@ RSpec.describe PetsController do
     end
 
     context 'when sending GET request to :index' do
-      it 'allows requests <= 4 per second' do
-        4.times { get :index }
+      it 'allows requests <= 5 per second' do
+        5.times { get :index }
         expect(response).to have_http_status(200)
       end
 
@@ -135,8 +135,8 @@ RSpec.describe PetsController do
     end
 
     context 'when sending GET request to :new' do
-      it 'allows requests <= 4 per second' do
-        4.times { get :new }
+      it 'allows requests <= 5 per second' do
+        5.times { get :new }
         expect(response).to have_http_status(200)
       end
 
@@ -148,13 +148,13 @@ RSpec.describe PetsController do
     context 'when sending GET request to :show' do
       let(:fake_pet) { double('fake_pet', id: 1) }
 
-      it 'allows requests <= 4 per second' do
+      it 'allows requests <= 5 per second' do
         allow(Pet).to receive(:find).and_return(fake_pet.id)
-        4.times { get :show, params: { id: fake_pet.id } }
+        5.times { get :show, params: { id: fake_pet.id } }
         expect(response).to have_http_status(200)
       end
 
-      it 'blocks requests > 6 per second' do
+      it 'blocks requests > 5 per second' do
         allow(Pet).to receive(:find).and_return(fake_pet.id)
         expect { 6.times { get :show, params: { id: fake_pet.id } } }.to output('Limit reached!').to_stdout
       end
@@ -163,13 +163,13 @@ RSpec.describe PetsController do
     context 'when sending GET request to :edit' do
       let(:fake_pet) { double('fake_pet', id: 1) }
 
-      it 'allows requests <= 4 per second' do
+      it 'allows requests <= 5 per second' do
         allow(Pet).to receive(:find).and_return(fake_pet.id)
-        4.times { get :edit, params: { id: fake_pet.id } }
+        5.times { get :edit, params: { id: fake_pet.id } }
         expect(response).to have_http_status(200)
       end
 
-      it 'blocks requests > 6 per second' do
+      it 'blocks requests > 5 per second' do
         allow(Pet).to receive(:find).and_return(fake_pet.id)
         expect { 6.times { get :edit, params: { id: fake_pet.id } } }.to output('Limit reached!').to_stdout
       end
@@ -178,13 +178,13 @@ RSpec.describe PetsController do
     context 'when sending GET request to :search' do
       let(:fake_search) { 'sa' }
 
-      it 'allows requests <= 4 per second' do
+      it 'allows requests <= 5 per second' do
         allow(Pet).to receive(:where).and_return(search: fake_search)
-        4.times { get :search, params: { search: fake_search } }
+        5.times { get :search, params: { search: fake_search } }
         expect(response).to have_http_status(302)
       end
 
-      it 'blocks requests > 6 per second' do
+      it 'blocks requests > 5 per second' do
         allow(Pet).to receive(:where).and_return(search: fake_search)
         expect { 6.times { get :search, params: { search: fake_search } } }.to output('Limit reached!').to_stdout
       end
