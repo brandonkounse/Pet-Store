89118e0f6705182186e5483dc27242a97d1249de
Brandon Kounse <kounse@gmail.com>
2023-09-21 20:23:37 -0500

    restructured stores controllerand set renders for JSON

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,13 +4,31 @@ class StoresController < ApplicationController
   extend Limiter::Mixin
   extend LimitHelper
 
+  MAX_PETS_PER_PAGE = 25
+
   limit(:index, :inventory)
 
   def index
-    @pet = Pet.order(:id).paginate(page: params[:page], per_page: 25)
+    @pet = Pet.order(:id).paginate(page: params[:page], per_page: MAX_PETS_PER_PAGE)
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          page: params.fetch(:page, 1),
+          number_of_pages: (Pet.count.to_f / MAX_PETS_PER_PAGE).ceil,
+          pets: @pet
+        }
+      end
+    end
   end
 
   def inventory
     @pet = Pet.all
+
+    render json: {
+      "sold": Pet.where(sold: true).count,
+      "available": Pet.where(sold: false).count
+    }
   end
 end
