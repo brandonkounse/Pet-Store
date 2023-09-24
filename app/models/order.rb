a26a8ada2d46c0e15fae6b5d5045a7de9712abb9
Brandon Kounse <kounse@gmail.com>
2023-09-24 10:58:34 -0500

    added validation to pet_id and custom error messaging. added check for pet sold on create to make sure order couldn't be made for a pet already purchased.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,11 @@ class Order < ApplicationRecord
   belongs_to :pet
 
   validates :user_email, email: true, presence: true
+  validates :pet_id,
+            numericality: { only_integer: true, greater_than: 0, message: 'ID must be a positive integer' },
+            presence: { message: "ID can't be blank" }
+
+  validate :pet_not_sold, on: :create
 
   before_create :generate_order_number, :set_order_date
   after_commit :toggle_sold
@@ -28,5 +33,9 @@ class Order < ApplicationRecord
 
   def toggle_sold
     pet.update(sold: !pet.sold)
+  end
+
+  def pet_not_sold
+    errors.add(:pet, 'is already sold!') if pet&.sold
   end
 end
